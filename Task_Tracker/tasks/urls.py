db5354cce84d74558918457e2e529635180add54
h-lakshman <lakshman.vaibhav007@gmail.com>
2023-11-13 17:45:42 +0530

    Created to function based views for bulk and bulk delete for easy understanding. They are implemented in different endpoints.

--- Task_Tracker/tasks/urls.py
+++ Task_Tracker/tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TaskListCreate, TaskDetail
+from .views import TaskListCreate, TaskDetail, bulk_add_tasks, bulk_delete_tasks
 
 urlpatterns = [
     path('tasks/', TaskListCreate.as_view()),
-    path('tasks/<int:id>', TaskDetail.as_view())
+    path('tasks/<int:id>', TaskDetail.as_view()),
+    path('tasks/bulk_add_tasks',bulk_add_tasks),
+    path('tasks/bulk_delete_tasks',bulk_add_tasks),
 ]
--- Task_Tracker/tasks/views.py
+++ Task_Tracker/tasks/views.py
@@ -1,12 +1,13 @@
 from .models import Task
 from .serializer import TaskSerializer
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
 from django.http import Http404
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-
+# This class-based view performs Listing and Creation of Tasks
 class TaskListCreate(APIView):
     def get(self, request):
         queryset = Task.objects.all()
@@ -21,6 +22,7 @@ class TaskListCreate(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# This class-based view performs getting a particular task,editing and deleting the task
 class TaskDetail(APIView):
     def get(self, request, id):
         try:
@@ -49,3 +51,33 @@ class TaskDetail(APIView):
         except Http404:
             return Response({'error': 'There is no task at that id'}, status=status.HTTP_404_NOT_FOUND)
 
+# This function-based view is used to bulk add tasks
+@api_view(['POST'])
+def bulk_add_tasks(request):
+    tasks = request.data.get('tasks', [])
+    created_tasks = []
+
+    for task in tasks:
+        serializer = TaskSerializer(data=task)
+        if serializer.is_valid():
+            task_save = serializer.save()
+            created_tasks.append({'id': task_save.id})
+
+    return Response({'tasks': created_tasks}, status=status.HTTP_201_CREATED)
+
+
+# This function-based view is used to bulk delete tasks
+@api_view(['DELETE'])
+def bulk_delete_tasks(request):
+    tasks = request.data.get('tasks', [])
+
+    for task in tasks:
+        task_id = task.get('id', None)
+        if task_id is not None:
+            try:
+                task_del = Task.objects.get(pk=task_id)
+                task_del.delete()
+            except Task.DoesNotExist:  # If handle doesnt exist, it does nothing
+                pass
+
+    return Response(status=status.HTTP_204_NO_CONTENT)
